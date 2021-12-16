97ef87cbf97a6c551b27e47e32e29ae941606d07
hussein.hasan <hussein.hasan@leaguesofcode.com>
2021-12-16 08:42:56 +0100

    c2

--- script.js
+++ script.js
@@ -13,12 +13,19 @@ const COLORS = {
 
 const PI = Math.PI;
 
+const wireframeMaterial = new THREE.MeshBasicMaterial({
+  color: "white",
+  wireframe: true,
+});
+
 // --- SCENE
  
 const scenes = {
   real: new THREE.Scene(),
   wire: new THREE.Scene(),
 };
+scenes.wire.overrideMaterial = wireframeMaterial;
+
 let size = { width: 0, height: 0 };
 
 scenes.real.background = new THREE.Color(COLORS.background);
@@ -109,16 +116,38 @@ onResize();
 
 // --- TICK
 
+const points = [];
+
+const clamp = (min, value, max) => Math.max(Math.min(max, value), min);
+
+const updatePoints = () => {
+  points.forEach((point) => {
+    const screenPosition = point.mesh.position.clone();
+    screenPosition.setFromMatrixPosition(point.mesh.matrixWorld);
+    screenPosition.project(views[0].camera);
+
+    // const translateX = clamp(0, screenPosition.x + 0.5, 1)
+    // const translateY = clamp(0, -screenPosition.y + 0.5, 1)
+
+    const translateX = clamp(-50, screenPosition.x * 100 * 0.5, 50);
+    const translateY = clamp(-50, -screenPosition.y * 100 * 0.5, 50);
+
+    point.node.style.setProperty("--x", translateX);
+    point.node.style.setProperty("--y", translateY);
+  });
+};
+
 const tick = () => {
   views.forEach((view) => {
     view.camera.lookAt(cameraTarget);
-    let bottom = view.bottom * size.height;
-    let height = view.height * size.height;
-    renderer.setViewport(0, bottom, size.width, height);
+    let bottom = size.height * view.bottom;
+    let height = size.height * view.height;
+    renderer.setViewport(0, 0, size.width, size.height);
     renderer.setScissor(0, bottom, size.width, height);
     renderer.setScissorTest(true);
     renderer.render(view.scene, view.camera);
   });
+  updatePoints();
   window.requestAnimationFrame(() => tick());
 };
 
@@ -126,8 +155,18 @@ tick();
 
 const models = {};
 const clones = {};
+let cameras = null;
+let rockets = null;
+let trees = null;
+
+const hideLoading = () => {
+  document.body.classList.add("ready");
+  gsap.to(".loading", { y: "-150%", ease: "power4.in", duration: 0.4 });
+};
 
 const setupAnimation = () => {
+  hideLoading();
+
   console.log(models);
   models.rocket.position.x = 5;
   models.tree.position.x = -5;
@@ -159,11 +198,14 @@ const desktopAnimation = () => {
   section++;
   tl.to(models.rocket.position, { z: 2 }, section);
   tl.to(models.tree.position, { x: -5, ease: "power4.in" }, section);
+  // tl.to(views[1], { height: 1, ease: "none" }, section);
 
   // section 3
   section++;
   tl.to(models.rocket.position, { z: -1, y: 4 }, section);
   tl.to(models.tree.position, { x: -5, ease: "power4.in" }, section);
+  // tl.to(views[1], { height: 1, ease: "none" }, section);
+
 };
 
 const loadManager = new THREE.LoadingManager(() => {
